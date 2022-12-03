d7543e969a19c51a67ec028e351af8dd2e881ff5
Antti Ruonakoski <aruonakoski@gmail.com>
2022-12-03 03:14:20 +0200

    major refactor. add default options. add download by recorded year option

---
+++ downloader/tiiradl.py
@@ -0,0 +1,205 @@
+from dataclasses import dataclass, asdict
+import time
+import sys
+import pickle
+import re
+from requests import Session
+from datetime import datetime, timedelta
+from os import path
+
+
+class Downloader:
+    def __init__(self, credentials: dict, options: dict) -> None:
+
+        if not credentials:
+            raise ValueError("Kirjatumistunnukset puuttuvat")
+        if not options:
+            options = DefaultOptions().get()
+        self.credentials = credentials
+        self.options = options
+
+    def get_session(self):
+        with Session() as sess:
+            try:
+                if time.time() - path.getmtime(absname("cookiefile")) > 600:
+                    print(
+                        datetime.now(),
+                        "\tYli 10 minuuttia vanha istunto. Kirjaudutaan uuteen istuntoon.",
+                    )
+                    self.login(self.credentials, sess)
+                    with open(absname("cookiefile"), "wb") as f:
+                        pickle.dump(sess.cookies, f)
+                else:
+                    with open(absname("cookiefile"), "rb") as f:
+                        cookies = pickle.load(f)
+                        sess.cookies = cookies
+            except IOError as e:
+                print(
+                    datetime.now(),
+                    "\tEi tiedostoa 'cookiefile'. Kirjaudutaan uuteen istuntoon.",
+                    e,
+                )
+                self.login(self.credentials, sess)
+                with open(absname("cookiefile"), "wb") as f:
+                    pickle.dump(sess.cookies, f)
+        self.session = sess
+        return sess
+
+    def login(self, credentials: dict, session: Session) -> None:
+        # kirjaudutaan ja noudetaan eväste
+        # Va,ka blu-users. Connect ja send:
+        url = self.options["URL"] + "/index.php"
+        post_data = {
+            "tunnus": credentials["TUNNUS"],
+            "salasana": credentials["SALASANA"],
+            "login": "KIRJAUDU",
+        }
+        try:
+            session.post(url, data=post_data, timeout=10)
+            print(datetime.now(), "\tKirjauduttu ja eväste tallennettu")
+        except Exception as e:
+            print("Kirjautuminen epäonnistui", e)
+            sys.exit(1)
+
+    def download_last_days_period(self, days: int, alue="23"):
+
+        now = datetime.now()
+        delta = timedelta(days=days)
+        start = now - delta
+        TALLENNUS_ALKUPVM = f"{start:%d.%m.%Y}"
+        TALLENNUS_LOPPUPVM = ""
+        ALUE = f"{alue}"
+
+        self.print_start_info()
+
+        url_parts = [
+            self.options["URL"],
+            self.options["ALKU"],
+            "kunta=",
+            self.options["KUNTA"],
+            "&paikka=",
+            self.options["PAIKKA"],
+            "&paivamaara=3",  # 3=tallennuspvm, 1=havaintopvm?
+            "&paivamaara_a=",
+            self.options["ALKUPVM"],
+            "&paivamaara_l=",
+            self.options["LOPPUPVM"],
+            "&paivamaara_tal_a=",
+            TALLENNUS_ALKUPVM,
+            "&paivamaara_tal_l=",
+            TALLENNUS_LOPPUPVM,
+            "&alue=",
+            ALUE,
+            "&qorde=&valinta=&omatilm=&omathav=&yhdistyslataus=1&yksmaara=&fi_rari=&fi_aika=&fi_maara=&al_rari=&\
+            al_aika=&al_maara=&piilota_poistetut=on&piilota_koontialkuper=on",
+            "&rajoitus=",
+            self.options["RAJOITUS"],
+            "&atlaskrakki=&lyhenne=nimi&taytto=kylla&summarivi=ei",
+        ]
+        url = "".join(url_parts)
+        self.url = url
+        return url
+
+    def download_period(self):
+
+        self.print_start_info()
+
+        url_parts = [
+            self.options["URL"],
+            self.options["ALKU"],
+            "kunta=",
+            self.options["KUNTA"],
+            "&paikka=",
+            self.options["PAIKKA"],
+            "&paivamaara=3",  # 3=tallennuspvm, 1=havaintopvm?
+            "&paivamaara_a=",
+            self.options["ALKUPVM"],
+            "&paivamaara_l=",
+            self.options["LOPPUPVM"],
+            "&paivamaara_tal_a=",
+            self.options['TALLENNUS_ALKUPVM'],
+            "&paivamaara_tal_l=",
+            self.options['TALLENNUS_LOPPUPVM'],
+            "&alue=",
+            self.options['ALUE'],
+            "&qorde=&valinta=&omatilm=&omathav=&yhdistyslataus=1&yksmaara=&fi_rari=&fi_aika=&fi_maara=&al_rari=&\
+            al_aika=&al_maara=&piilota_poistetut=on",
+            "&piilota_koontialkuper=",
+            self.options['PIILOTA_KOONTIALKUPER'],
+            "&rajoitus=",
+            self.options["RAJOITUS"],
+            "&atlaskrakki=&lyhenne=nimi&taytto=kylla&summarivi=ei",
+        ]
+        url = "".join(url_parts)
+        self.url = url
+        return url
+
+    def download(self) -> str:
+        try:
+            csv_raw = self.session.get(self.url)
+            csv_raw.raise_for_status()
+        except Exception as e:
+            print("Virhe csv-tiedoston osoitteen hakemisessa. \n", e)
+            sys.exit(1)
+
+        filepattern = r"omatcsvt\/.*.txt"
+        downloadable_file = re.findall(
+            filepattern, csv_raw.text
+        )  # omatcsvt/124_csv_YJ4ofuUwsp.txt
+        url = self.options["URL"] + "/" + downloadable_file[0]
+        print(f"Lopullinen latausurl: {url}")
+
+        try:
+            csv = self.session.get(self.url)
+            csv.raise_for_status()
+        except Exception as e:
+            print("Virhe csv-tiedoston latauksessa. \n", e)
+            sys.exit(1)
+        return csv.text
+
+    def print_start_info(self):
+        print(
+            datetime.now(),
+            "\talkupvm ",
+            self.options["TALLENNUS_ALKUPVM"],
+            "loppupvm ",
+            self.options["TALLENNUS_LOPPUPVM"],
+        )
+
+
+@dataclass
+class DefaultOptions:
+    URL: str = "https://www.tiira.fi"
+    ALKU: str = "/csv_omat.php?laji=&valtkun=&"
+    TALLENNUS_ALKUPVM: str = ""
+    TALLENNUS_LOPPUPVM: str = ""
+    ALKUPVM: str = ""
+    LOPPUPVM: str = ""
+    KUNTA: str = ""
+    PAIKKA: str = ""
+    RAJOITUS: str = "5"  # '0' kaikki, '5' salaamattomat
+    ALUE: str = "23"  # Lapin lintutieteellinen yhdistys
+    PIILOTA_KOONTIALKUPER: str = "on"
+
+    def get(self):
+        return asdict(self)
+
+
+@dataclass
+class YearOptions(DefaultOptions):
+    year: int = datetime.now().year
+    PIILOTA_KOONTIALKUPER: str = ""
+
+    def __post_init__(self):
+        self.TALLENNUS_ALKUPVM = "1.1." + str(self.year)
+        self.TALLENNUS_LOPPUPVM = "31.12." + str(self.year)
+        self.PIILOTA_KOONTIALKUPER = ''
+
+
+def absname(filename) -> str:
+    cp = path.dirname(path.abspath(__file__))
+    return path.join(cp, filename)
+
+
+if __name__ == "__main__":
+    pass
--- downloader/tiiradownloader.py
+++ downloader/tiiradownloader.py
@@ -4,7 +4,7 @@
 """tiiradownloader.py: Authenticate and download a csv-file from tiira.fi."""
 
 __author__ = "Antti Ruonakoski"
-__copyright__ = "Copyright 2018"
+__copyright__ = "Copyright 2018-2022"
 __credits__ = ["Antti Ruonakoski"]
 __license__ = "MIT"
 __version__ = ""
@@ -12,81 +12,14 @@ __maintainer__ = "Antti Ruonakoski"
 __email__ = "aruonakoski@gmail.com"
 __status__ = "Development"
 
-from os import remove, path
 import time
 import sys
 import html
-import re
-import requests
-import pickle
 import argparse
-from datetime import datetime, timedelta
+from datetime import datetime
+from tiiradl import Downloader, DefaultOptions, YearOptions, absname
 
 wait = 5
-cp = path.dirname(path.abspath(__file__))
-
-
-def absname(filename):
-    return path.join(cp, filename)
-
-
-URL = 'https://www.tiira.fi'
-ALKU = '/csv_omat.php?laji=&valtkun=&'
-
-HAVALKUPVM = ''
-HAVLOPPUPVM = ''
-TALLENNUS_ALKUPVM = ''
-TALLENNUS_LOPPUPVM = ''
-ALKUPVM = ''
-LOPPUPVM = ''
-KUNTA = ''
-PAIKKA = ''
-RAJOITUS = '5'  # '0' kaikki, '5' salaamattomat
-ALUE = '23'  # Lapin lintutieteellinen yhdistys
-
-# ALKUVUOSI = sys.argv[1]
-# LOPPUVUOSI = sys.argv[2]
-
-days_past = 7  # default downloaded period in days
-
-
-def login(credentials, session):
-
-    # kirjaudutaan ja noudetaan eväste
-    # Va,ka blu-users. Connect ja send:
-    url = URL + '/index.php'
-    post_data = {
-        'tunnus': credentials['TUNNUS'],
-        'salasana': credentials['SALASANA'],
-        'login': 'KIRJAUDU'
-    }
-    try:
-        r = session.post(url, data=post_data, timeout=10)
-        print(datetime.now(), '\tKirjauduttu ja eväste tallennettu')
-        return r.cookies
-    except Exception as e:
-        print('Kirjautuminen epäonnistui', e)
-        sys.exit(1)
-
-
-def new_session(credentials):
-    s = requests.session()
-    try:
-        if time.time() - path.getmtime(absname('cookiefile')) > 600:
-            print(datetime.now(), '\tYli 10 minuuttia vanha istunto. Kirjaudutaan uuteen istuntoon.')
-            login(credentials, s)
-            with open(absname('cookiefile'), 'wb') as f:
-                pickle.dump(s.cookies, f)
-        else:
-            with open(absname('cookiefile'), 'rb') as f:
-                cookies = pickle.load(f)
-                s.cookies = cookies
-    except IOError as e:
-        print(datetime.now(), '\tEi tiedostoa \'cookiefile\'. Kirjaudutaan uuteen istuntoon.', e)
-        login(credentials, s)
-        with open(absname('cookiefile'), 'wb') as f:
-            pickle.dump(s.cookies, f)
-    return s
 
 # TBD
 
@@ -100,111 +33,112 @@ def new_session(credentials):
 #         print ("alkupvm ", TALLENNUS_ALKUPVM, "loppupvm ", TALLENNUS_LOPPUPVM)
 
 
-def download_period(days, session, alue='23'):
+def main(
+    days: int,
+    filename: str,
+    bl_area: int,
+    year_recorded=None,
+    start_date=None,
+    end_date=None,
+    include_hidden=False
+):
 
-    now = datetime.now()
-    delta = timedelta(days=days)
-    start = now - delta
-    TALLENNUS_ALKUPVM = f'{start:%d.%m.%Y}'
-    TALLENNUS_LOPPUPVM = ''
-    ALUE = f'{alue}'
+    if (include_hidden):
+        RAJOITUS = '0'
+    else:
+        RAJOITUS = '5'
+    csv_filename = filename
+    credentials = {"TUNNUS": "", "SALASANA": ""}
 
-    print(datetime.now(), "\talkupvm ", TALLENNUS_ALKUPVM, "loppupvm ", TALLENNUS_LOPPUPVM)
-
-    url_parts = [
-        URL,
-        ALKU,
-        'kunta=', KUNTA,
-        '&paikka=', PAIKKA,
-        '&paivamaara=3',  # 3=tallennuspvm, 1=havaintopvm?
-        '&paivamaara_a=', ALKUPVM,
-        '&paivamaara_l=', LOPPUPVM,
-        '&paivamaara_tal_a=', TALLENNUS_ALKUPVM,
-        '&paivamaara_tal_l=', TALLENNUS_LOPPUPVM,
-        '&alue=', ALUE,
-        '&qorde=&valinta=&omatilm=&omathav=&yhdistyslataus=1&yksmaara=&fi_rari=&fi_aika=&fi_maara=&al_rari=&\
-        al_aika=&al_maara=&piilota_poistetut=on&piilota_koontialkuper=on',
-        '&rajoitus=', RAJOITUS,
-        '&atlaskrakki=&lyhenne=nimi&taytto=kylla&summarivi=ei']
-    url = ''.join(url_parts)
-
-    try:
-        csv_raw = session.get(url)
-    except Exception as e:
-        print(e)
-
-    filepattern = 'omatcsvt\/.*.txt'
-    downloadable_file = (re.findall(filepattern, csv_raw.text))  # omatcsvt/124_csv_YJ4ofuUwsp.txt
-    url = URL + '/' + downloadable_file[0]
-    print('lopullinen latausurl ', url)
-
-    try:
-        csv = session.get(url)
-    except Exception as e:
-        print(e)
-    return csv.text
-
-
-def main(days_past, csv_filename, alue):
-
-    credentials = {
-            'TUNNUS': '',
-            'SALASANA': ''
-        }
-
-    with open(absname('credentials.txt'), 'r') as fp:
-        for i in ['TUNNUS', 'SALASANA']:
+    with open(absname("credentials.txt"), "r") as fp:
+        for i in ["TUNNUS", "SALASANA"]:
             credentials[i] = fp.readline().strip()
 
-    session = new_session(credentials)
+    dl = Downloader(credentials=credentials, options={})
 
-    for chance in range(3):
+    if year_recorded:
+        dl.options = YearOptions(ALUE=str(bl_area), year=year_recorded, RAJOITUS=RAJOITUS).get()
+        dl.download_period()
+        print(dl.options)
+        csv_filename = f"{csv_filename.split('.')[0]}_tallenusvuosi_{year_recorded}.csv"
+        print(csv_filename)
+    elif days:
+        dl.options = DefaultOptions(ALUE=str(bl_area), RAJOITUS=RAJOITUS).get()
+        dl.download_last_days_period(days)
+    else:
+        print("Ei tallennusvalintoja")
+        sys.exit(1)
+
+    dl.get_session()
+
+    for chance in range(2):
         try:
-            print(datetime.now(), f'\tLadataan csv-tiedosto {days_past} päivän ajalta')
-            csv = download_period(days_past, session, alue)
-            if len(csv.split('\r\n', 1)[0]) < 380 or len(csv.split('\r\n', 1)[0]) > 410:
+            print(datetime.now(), f"\tLadataan csv-tiedosto {days} päivän ajalta")
+            csv = dl.download()
+            if len(csv.split("\r\n", 1)[0]) < 380 or len(csv.split("\r\n", 1)[0]) > 410:
                 # LRCF newlines
                 # normal tiira csv-file first line length for 1st line is 391, depends on linefeed characters.
                 # allows minor csv-format change
                 print(
                     datetime.now(),
-                    '\tViallinen csv-tiedosto tai vanhentunut istunto. Kirjaudutaan ja yritetään uudelleen. kerta: ',
-                    chance
-                    )
+                    "\tViallinen csv-tiedosto tai vanhentunut istunto. Kirjaudutaan ja yritetään uudelleen. kerta: ",
+                    chance,
+                )
                 time.sleep(wait)
-                session = new_session(credentials)
+                dl.get_session()
             else:
-                with open(csv_filename, 'w', newline='\n') as f:
+                with open(csv_filename, "w", newline="\n") as f:
                     csv_unescaped = html.unescape(csv)
-                    csv_unix_newlines = '\n'.join(csv_unescaped.splitlines())
+                    csv_unix_newlines = "\n".join(csv_unescaped.splitlines())
                     f.write(csv_unix_newlines)
                     # converts newlines
                     # converts to utf-8 because it's native text write encoding in Python.
                     # html entities such as emojis should be unescaped
                     # tbd check malformed rows (extra '#' inputted in tiira text field).
                     # doesn't matter with pandas csv impoter, but effects SQL copy commands
-                    print(datetime.now(), '\tcsv-tiedosto tallennettu onnistuneesti')
+                    print(datetime.now(), "\tcsv-tiedosto tallennettu onnistuneesti")
                 break
         except Exception as e:
-            print(f'error {e}')
+            print(f"error {e}")
             sys.exit(1)
-    print(datetime.now(), '\tLataus päättyi')
+    print(datetime.now(), "\tLataus päättyi")
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Kirjaantuu Tiiraan ja lataa csv-tiedoston tallennusajan mukaan")
+    parser = argparse.ArgumentParser(
+        description="Kirjaantuu Tiiraan ja lataa csv-tiedoston tallennusajan mukaan."
+    )
     parser.add_argument(
-        "-d", "--days", type=int, help="kuinka monta päivää taaksepäin nykyhetkestä  (oletus 7)",
-        default=7
-        )
+        "-yr",
+        "--year-recorded",
+        type=int,
+        help="Ladataan havainnot yhdeltä tallennusvuodelta (oletus kuluva vuosi)",
+    )
     parser.add_argument(
-        "-f", "--filename", type=str, help="ladattavan tiedoston nimi (oletus tiira.csv)",
-        default="tiira.csv"
-        )
+        "-d",
+        "--days",
+        type=int,
+        help="Ladataan havainnot kuluvasta päivästä annettu määrä päiviä taaksepäin. (oletus 7)",
+    )
     parser.add_argument(
-        "-a", "--area", type=int, help="Lintutieteellisen yhdistyksen aluekoodi (oletus 23)",
-        default="23"
-        )
+        "--include-hidden",
+        action='store_true',
+        help="Salatut sisältyvät lataukseen (oletus ei).",
+    )
+    parser.add_argument(
+        "-f",
+        "--filename",
+        type=str,
+        help="Tallennettavan csv-tiedoston nimi (oletus tiira.csv)",
+        default="tiira_lly.csv",
+    )
+    parser.add_argument(
+        "-a",
+        "--bl-area",
+        type=int,
+        help="Lintutieteellisen yhdistyksen aluekoodi (oletus 23)",
+        default="23",
+    )
     args = parser.parse_args()
-    main(days_past=args.days, csv_filename=args.filename, alue=args.area)
+    main(**vars(args))
